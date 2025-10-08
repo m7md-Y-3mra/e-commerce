973c1b955e5b9a7cc6017140d89ae0b5d6ff4d9b
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-10-08 15:41:31 +0300

    chore(Navbar): remove commented-out CSS class strings

--- src/components/Header/components/Navbar/index.tsx
+++ src/components/Header/components/Navbar/index.tsx
@@ -4,9 +4,6 @@ import { navItems } from "../NavLink/constant";
 import type { NavbarProps } from "./types";
 
 const Navbar: FC<NavbarProps> = ({ style, type }) => {
-  //md:hidden mt-4 pb-4 flex flex-col gap-4 text-center
-  //md:hidden mt-4 pb-4 flex flex-col gap-4 text-center
-  //hidden md:flex items-center gap-8
   return (
     <nav className={style}>
       {navItems.map((item) => (
