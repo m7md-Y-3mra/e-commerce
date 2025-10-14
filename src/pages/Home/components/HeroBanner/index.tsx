d6308b3b838d3741a85313fe989047fb0ffd1c94
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-10-14 14:20:51 +0300

    style(HeroBanner): add bottom padding to hero section

--- src/pages/Home/components/HeroBanner/index.tsx
+++ src/pages/Home/components/HeroBanner/index.tsx
@@ -5,7 +5,7 @@ import type { HeroBannerProps } from "./types";
 
 export default function HeroBanner({ className }: HeroBannerProps) {
   return (
-    <section className={cn("container mx-auto", className)}>
+    <section className={cn("container mx-auto pb-8", className)}>
       <div className="flex flex-col lg:flex-row gap-4 sm:gap-6 lg:gap-8">
         {/* Categories Sidebar */}
         <CategoriesSidebar />
